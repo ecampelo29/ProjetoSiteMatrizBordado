9e47d67e0cdc3af2d03a7803782aaa2129a70d94
Emerson Campelo <ecampelo29@gmail.com>
2022-04-15 14:55:53 -0300

    paginacao funciona parcialmente

--- scripts/Controllers/listaMatrizes.js
+++ scripts/Controllers/listaMatrizes.js
@@ -3,26 +3,27 @@
 import { matrizService } from "../Service/cliente-service.js";
 import { criaNovoCartao } from "./criaCartao.js";
 
-const pagAtual = 1;
+//const pagAtual = 1;
 const cartoesPorPagina = 12;
 
-const pagAnterior =()=>{
-    if (pagAtual > 1) {
-        pagAtual--;
-        renderizaPagina(pagAtual);
-    }
-}
+// const pagAnterior =()=>{
+//     if (pagAtual > 1) {
+//         pagAtual--;
+//         renderizaPagina(pagAtual);
+//     }
+// }
 
-const proximaPagina = ()=>{
-    if (pagAtual < numeroPaginas()) {
-        pagAtual++;
-        renderizaPagina(pagAtual);
-    }
-}
+// const proximaPagina = ()=>{
+//     if (pagAtual < numeroPaginas()) {
+//         pagAtual++;
+//         renderizaPagina(pagAtual);
+//     }
+// }
 
 const numeroPaginas=(totalCartoes)=>{
     return Math.ceil(totalCartoes / cartoesPorPagina);
 }
+
 
 //const listaDeCartoes = document.querySelector('[data-cartoes]')
 
@@ -30,42 +31,56 @@ const renderizaCartoes = async (nrDaPag) => {
     /* função que renderiza os cartões existentes na página */
     const btn_anterior = document.getElementById("btn_anterior");
     const btn_proxima = document.getElementById("btn_proxima");
-    const listaDeCartoes = document.querySelector('[data-cartoes]')
-    var page_span = document.getElementById("pagina");
+    const listaDeCartoes = document.querySelector('[data-cartoes]');
     
    try {
-       const listarMatrizes = await matrizService.listaMatrizes()
-       const totalCartoes = listarMatrizes.length
+       const listarMatrizes = await matrizService.listaMatrizes();
+       const totalCartoes = listarMatrizes.length;
+       const nrPaginas = numeroPaginas(totalCartoes);
 
    // Validate page
         if (nrDaPag < 1) nrDaPag = 1;
-        if (nrDaPag > numeroPaginas(totalCartoes)) nrDaPag = numeroPaginas(totalCartoes);
-        console.log(nrDaPag)
+        if (nrDaPag > nrPaginas) nrDaPag = nrPaginas;
+
         listaDeCartoes.innerHTML = "";
 
-        for (var i = (nrDaPag-1) * cartoesPorPagina; i < (nrDaPag * cartoesPorPagina) && i < totalCartoes; i++) {            
+        for (let i = (nrDaPag-1) * cartoesPorPagina; i < (nrDaPag * cartoesPorPagina) && i < totalCartoes; i++) {            
             let matriz = listarMatrizes[i]
             let cartao = criaNovoCartao(matriz.image_location, matriz.name, matriz.last_price, matriz.actual_price)
             listaDeCartoes.appendChild(cartao)
             //listaDeCartoes.appendChild(criaNovoCartao(matriz.image_location, matriz.name, matriz.last_price, matriz.actual_price))
         }
       
-        page_span.innerHTML = nrDaPag + "/" + numeroPaginas(totalCartoes);
-        
-        if (nrDaPag == 1) {
-            btn_anterior.style.visibility = "hidden";
-        } else {
-            btn_anterior.style.visibility = "visible";
-        }
-        
-        if (nrDaPag == numeroPaginas(totalCartoes)) {
-            btn_proxima.style.visibility = "hidden";
-        } else {
-            btn_proxima.style.visibility = "visible";
-        }
+        return totalCartoes
+                
     }
     catch (erro){
             console.log(erro)
     }
 }
-renderizaCartoes(1)
+
+const qtdCartoes = renderizaCartoes(1)
+
+// com o retorno da promessa processa a quantidade de cartões por página
+qtdCartoes.then(function(totalCartoes){
+    const totalPaginas = numeroPaginas(totalCartoes);
+    numeracaoPaginas(totalPaginas)
+})
+
+const numeracaoPaginas = (totalPaginas) =>{
+    /* função que inclui a numeração de paginas  */
+    const paginacao = document.querySelector('[data-paginas]');
+    const raquo = paginacao.lastElementChild;
+
+    for (let i = 1; i <= totalPaginas; i++) {        
+        const listaPag = document.createElement('li')
+        listaPag.onclick = function() {
+            renderizaCartoes(this.dataset.id)
+        }
+        listaPag.innerHTML = `<a class="matrizes__paginacao__lista__pagina">${i}</a>`
+        listaPag.dataset.id= i;
+        paginacao.insertBefore(listaPag, raquo)        
+    }
+}
+
+
