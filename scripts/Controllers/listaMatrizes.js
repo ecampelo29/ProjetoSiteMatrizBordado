f898fcc9a1fa5f460e82d6e7356e5e393d451bd2
Emerson Campelo <ecampelo29@gmail.com>
2022-05-14 09:17:20 -0300

    atualizando arquivos

--- scripts/Controllers/listaMatrizes.js
+++ scripts/Controllers/listaMatrizes.js
@@ -1,22 +1,22 @@
-//import { matrizService } from "../api.js"
+// //import { matrizService } from "../api.js"
 
-import { matrizService } from "../Service/cliente-service.js";
-import { criaNovoCartao } from "./criaCartao.js";
+// import { matrizService } from "../Service/cliente-service.js";
+// import { criaNovoCartao } from "./criaCartao.js";
 
-const listaDeCartoes = document.querySelector('[data-cartoes]')
-let cartoesPorPagina = 12;
+// const listaDeCartoes = document.querySelector('[data-cartoes]')
+// let cartoesPorPagina = 12;
 
-// const renderizaCartoes = async () => {
-//     /* função que renderiza os cartões existentes na página */
-//     try {
-//         const listarMatrizes = await matrizService.listaMatrizes()
-//         listarMatrizes.forEach(matriz => {
-//             listaDeCartoes.appendChild(criaNovoCartao(matriz.image_location, matriz.name, matriz.last_price, matriz.actual_price))
-//         })
-//     }
-//     catch (erro){
-//         console.log(erro)
-//     }
-// }
-// renderizaCartoes()
+// // const renderizaCartoes = async () => {
+// //     /* função que renderiza os cartões existentes na página */
+// //     try {
+// //         const listarMatrizes = await matrizService.listaMatrizes()
+// //         listarMatrizes.forEach(matriz => {
+// //             listaDeCartoes.appendChild(criaNovoCartao(matriz.image_location, matriz.name, matriz.last_price, matriz.actual_price))
+// //         })
+// //     }
+// //     catch (erro){
+// //         console.log(erro)
+// //     }
+// // }
+// // renderizaCartoes()
 
---
+++ scripts/Controllers/renderizaPaginas.js
@@ -0,0 +1,88 @@
+
+import { matrizService } from "../Service/cliente-service.js";
+import { criaNovoCartao } from "./criaCartao.js";
+import { paginacao } from "./nrPagina.js";
+
+const listagemDeCartoes = document.getElementById('listagemDeCartoes');
+let pagina = 0;
+globalThis.qtdPaginas = 0;
+const cartoesPorPagina = 2;
+
+
+function totalPaginas(totalRegistros){ 
+    globalThis.qtdPaginas = Math.ceil(totalRegistros / cartoesPorPagina);
+    return globalThis.qtdPaginas;
+    }
+
+function criaPagina (pagina){
+    // div para comportar as ul
+
+    const elementoPagina = document.createElement('div');
+    elementoPagina.id = paginacao.identificaPagina(pagina);
+    elementoPagina.className = 'matrizes__pagina';
+    // inclusão da ul que receberá a lista de matrizes
+    elementoPagina.innerHTML = `<ul class="matrizes__cartoes" data-cartoes> </ul>` 
+    const paginaPopulada = populaPagina(pagina, elementoPagina.firstChild)
+
+    const resultado =  paginaPopulada.then(function(value){       
+        if(pagina <= totalPaginas(value)){
+            return elementoPagina 
+        }
+        else {
+            return null
+        }
+    })
+    return resultado          
+}
+
+const populaPagina = async (pagina, listaDeCartoes) => { 
+    try {
+         const listarMatrizes = await matrizService.listaMatrizes()
+
+         for(let i = (pagina -1) * cartoesPorPagina; i < (pagina * cartoesPorPagina) && i < listarMatrizes.length; i++){            
+             const matriz = listarMatrizes[i]
+             const novoCartao = criaNovoCartao(matriz.image_location, matriz.name, matriz.last_price, matriz.actual_price)
+             listaDeCartoes.appendChild(novoCartao)         
+         }
+         return listarMatrizes.length
+    }
+    catch (erro) {
+        console.log('populaPagina:', erro)
+    }
+    
+}
+
+const renderizaPagina = (pagina) => {
+    return new Promise (async function(resolve,reject){
+        const novaPagina = await criaPagina(pagina);
+        if (typeof(novaPagina) === 'object' && novaPagina !== null) {       
+            listagemDeCartoes.appendChild(novaPagina);
+            resolve();
+        } 
+        else {
+            reject()
+        }
+
+    })
+};
+
+
+const novaPagina =  (pagina)=> {
+    const render = renderizaPagina(pagina);
+    render.then (function () {
+        paginacao.adicionaNrPagina(pagina);
+    }).catch(function(erro){
+        console.log('Página não foi criada: ', erro)
+    })
+}
+
+
+novaPagina(++pagina)
+
+const botaoAdicionaPagina = document.getElementById("carregaPagina");
+
+botaoAdicionaPagina.onclick = (function(){
+  if (pagina >= window.qtdPaginas) return; 
+  if (pagina+1 >= window.qtdPaginas) this.classList.add("carrega_pagina--inativo");     
+  novaPagina(++pagina)
+})
